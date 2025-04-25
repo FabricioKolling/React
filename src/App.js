804b937647c7a5fa6cb777d8a0fc3c833013b37e
Fabricio Kolling <162700727+FabricioKolling@users.noreply.github.com>
2025-04-25 11:00:53 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Frase from './Frase'
+
+function App() {
+
+  const nome = 'João Pedro'
+  const idade = 8
+  function soma (a,b){
+    return a + b
+  }
+  const novoNome = nome.toUpperCase()
+
+  return (
+    <div className="App">
+      <h1>Página de teste</h1>
+      <h3>Nome: {novoNome}🔪🩸</h3>
+      <h3>Idade: {idade}🐣</h3>
+      <p>Soma: { soma(8,9) }</p>
+
+      <Frase/>
+    </div>
+  );
+}
+
+export default App;
